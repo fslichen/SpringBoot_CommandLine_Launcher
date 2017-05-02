32baaddb0d301ee5ea5bde9df930f67eece932dd
chenli <chenli@192.168.43.24>
2017-05-02 10:59:53 +0800

    new demos

--- Project/src/main/java/evolution/ApplicationLauncher.java
+++ Project/src/main/java/evolution/ApplicationLauncher.java
@@ -3,7 +3,13 @@ package evolution;
 public class ApplicationLauncher {
 	public static void main(String[] args) {
 		String[] arguments = new String[1];
-		arguments[0] = "--server.port=8001";// Overrides application.properties
+		if (System.currentTimeMillis() % 2 == 0) {
+			System.out.println("Use port number from application.properties.");
+			arguments[0] = "";
+		} else {
+			System.out.println("Use port number from command line.");
+			arguments[0] = "--server.port=8001";// Overrides application.properties
+		}
 		Application.main(arguments);
 	}
 }
